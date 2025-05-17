edb86676683096a57e63d1c7f13288c2fd1108a8
rokloc <roku.trlt@gmail.com>
2025-05-17 21:42:32 +0900

    1st

---
+++ src/sample/Main.java
@@ -0,0 +1,12 @@
+package sample;
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello, Java");
+		int i;
+		i = 0;
+		
+		System.out.println(++i);
+		System.out.println(i);
+		/*コメント*/
+	}
+}
