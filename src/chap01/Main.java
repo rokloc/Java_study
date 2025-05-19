78cdc30029d3a07832d254bf519be4df338823c7
rokloc <roku.trlt@gmail.com>
2025-05-20 06:56:42 +0900

    chap01

---
+++ src/chap01/Main.java
@@ -0,0 +1,43 @@
+
+/*
+ 1-1
+ コンパイラ
+ インタプリタ
+  ソースコード
+  バイトコード
+  JVM
+ */
+
+//1-2
+/*
+package chap01;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int a = 3;
+		int b = 5;
+		int c = a * b;
+		
+		System.out.println("縦幅" + a + "横幅" + b + "の長方形の面積は" + c);
+	}
+
+}
+*/
+
+//1-3
+package chap01;
+
+public class Main {
+
+	public static void main(String[] args) {
+		boolean ans = true;
+		char word = '駆';
+		double num = 3.14;
+		long num2 = 31446464615L;
+		String str = "ナルト、螺旋丸！";
+	}
+
+}
+
+
