bde7bfbb9c1b540f8f4677bf78b62ca71dd57bc6
rokloc <roku.trlt@gmail.com>
2025-05-25 21:07:22 +0900

    chap05

--- src/chap05/Main.java
+++ src/chap05/Main.java
@@ -18,15 +18,49 @@ public class Main {
 	}
 	*/
 	
-	//5-2
+	//5-2.5-3
 	public static void main(String[] args) {
-		String[] msg = {"メールの宛先アドレス", "メールのタイトル", "メールの本文"};
-		email(msg[0], msg[1], msg[2]);
+		//email("メールの宛先アドレス", "メールのタイトル", "メールの本文");//5-2
+		//email("メールの宛先アドレス" , "メールの本文");					//5-3
+		//5-4
+		//三角形の面積
+		System.out.println("三角形の面積求めるよ！まず三角形の底辺の値入力よろ！(単位 : cm)");
+		double bottom = new java.util.Scanner(System.in).nextDouble();
+		System.out.println("次に三角形の高さの値の入力よろ！(単位 : cm)");
+		double height = new java.util.Scanner(System.in).nextDouble();
+		double calcTriangleArea = calcTriangleArea(bottom, height);
+		System.out.println("三角形の面積は" + calcTriangleArea + "平方cmだね！\n");
+		
+		//円の面積
+		System.out.println("次に円の面積求めるよ！円の半径の値入力よろ！(単位 : cm)");
+		double radius = new java.util.Scanner(System.in).nextDouble();
+
+		double circleArea = calcCircleArea(radius);
+		System.out.println("その円の面積は" + circleArea + "平方cmだね！\n");
+		
 	}
-	
+	//5-2
 	public static void email(String address, String title, String text) {
 		System.out.println(address + "に、以下のメールを送信しました\n"
 				+ "件名:" + title + "\n" + "本文:" + text + "\n");
 	}
+	//5-3
+	public static void email(String address, String text){
+		System.out.println(address + "に、以下のメールを送信しました\n"
+				+ "件名 : 無題\n"
+				+ "本文 : " + text + "\n");
+	}
+	
+	//5-4
+	public static double calcTriangleArea(double bottom, double height) {
+		return (bottom * height / 2);
+	}
+	public static double calcCircleArea(double radius) {
+		return (radius * radius * 3.14);
+	}
+	
+	
+	
 
+	
 }
